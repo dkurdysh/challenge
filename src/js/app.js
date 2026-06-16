8b37bcbf9e997c825302cac2791b59cfaedea7d6
dkurdysh <dkurdysh@edu.hse.ru>
2026-06-16 14:45:53 +0300

    add header with mobile nav

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
-console.log('init new challenges app');
+console.log("init new challenges app");
+
+const navToggle = document.querySelector('[data-element~="navToggle"]');
+const body = document.body;
+
+const toggleNav = () => {
+  body.classList.toggle("is-nav-open");
+};
+
+if (navToggle) {
+  navToggle.addEventListener('click', toggleNav);
+}
